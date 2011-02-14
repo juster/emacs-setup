39bc80ada048c04b9a4531aa5ddbe59afd0e3139
Justin Davis <jrcd83@gmail.com>
2011-02-14 10:53:52 -0500

    Adds org-mode. Fixes ocaml file association.
    README is updated to mention org mode must be installed separately.
    .gitignore adds the org mode directory.

--- emacs-setup.el
+++ emacs-setup.el
@@ -11,7 +11,7 @@
               (mapcar (lambda (path)
                         (concat emacs-setup-dir "/" path))
                       '("egg" "yasnippet" "ergoemacs-keybindings"
-                        "ocaml"))
+                        "ocaml" "org/lisp" "org/contrib/lisp"))
 	      load-path))
 
 ;; ErgoEmacs -- just the keybindings, load us quick!
@@ -90,4 +90,16 @@
 
 ;; OCAML
 (require 'ocaml)
-(add-to-list 'auto-mode-alist '("\\.ml$" . caml-mode))
+(add-to-list 'auto-mode-alist '("\\.ml[yli]?$" . caml-mode))
+
+;; Org-mode
+(require 'org-install)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode ))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-hide-leading-stars t)
