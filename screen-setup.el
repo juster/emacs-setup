26b563526e3d5efeae099f7b12cbc61d58935e12
Justin Davis <jrcd83@gmail.com>
2011-03-23 08:14:51 -0700

    Remove screen-setup.el. I now use tmux. Updates README.

--- screen-setup.el
+++
@@ -1,9 +0,0 @@
-;; I have setup screen to start on login according to this article:
-;; http://taint.org/wk/RemoteLoginAutoScreen
-
-;; We create a new screen whenever emacsclient is used. This
-;; removes the old screen.
-(add-hook 'server-done-hook
-          (lambda ()
-	    (shell-command
-	     "screen -r -X select `cat ~/.emacsclient-caller`")))
